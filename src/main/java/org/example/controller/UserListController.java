8164562598ef29cc43ed4201604bbea9b4e4e93c
adela-domokosova <adela.domokosova@gmail.com>
2025-05-07 16:23:43 +0200

    feat: switching scenes

--- src/main/java/org/example/controller/UserListController.java
+++ src/main/java/org/example/controller/UserListController.java
@@ -9,21 +9,16 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HomeController {
+public class UserListController {
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    public void switchToUserList(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/userList.fxml"));
+    public void switchToHome(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/home.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-
-    public void handleButtonClick(ActionEvent actionEvent) {
-        System.out.println("hello");
-    }
-
 }

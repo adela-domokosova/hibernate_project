49dc09a9aad5f9103c5d60b5fd6437b3ceeb2556
adela-domokosova <adela.domokosova@gmail.com>
2025-05-18 10:49:04 +0200

    added entity manager to Main class and logback.xml file with stdout config

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.EntityManagerFactory;
+
 //zde budu vyrábět ty entyti manaery
 public class Main extends Application {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    private static EntityManagerFactory EMF;
+
     public static void main(String[] args) {
         launch(args);
     }

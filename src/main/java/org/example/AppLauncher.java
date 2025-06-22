825587de2e04aaff57ad0e756af4e565b93c1e84
adela-domokosova <adela.domokosova@gmail.com>
2025-06-22 02:02:22 +0200

    feat: add AppLauncher class and configure Maven build with assembly and compiler plugins

---
+++ src/main/java/org/example/AppLauncher.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import javafx.application.Application;
+public class AppLauncher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
